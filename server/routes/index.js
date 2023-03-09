c3d4fb53584a3cdfbbff43925046c6ff56739f2f
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-09 13:46:17 -0600

    Added wrong route to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,11 @@ const path = require('path');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use((req, res) => res.send('Wrong route!'));
 
 // serve up react front-end in production
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
-module.exports = router;
+module.exports = router; 

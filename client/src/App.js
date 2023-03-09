98717392db0c536f9c7456d1893cf672ee2a5104
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-09 15:44:12 -0500

    signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Signup from './components/Signup';
 import React from 'react';
 // import Footer from './components/Footer';
 
 function App() {
   return (
-<Header />
+    <div>
+      <Header />
+      <Signup />
+    </div>
   );
 }
 

0573e37e80e1bb6595c10631bc74d6cd9dfa55fc
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-10 09:50:19 -0600

    Added email validation/username unique to user model

--- server/models/User.js
+++ server/models/User.js
@@ -4,8 +4,13 @@ const commentSchema = require('./Comment');
 const replySchema = require('./Reply');
 
 const userSchema = new Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, validate: {
+        validator: function(v) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+        },
+        message: props => `${props.value} is not a valid email address!`
+    }, },
     password: { type: String, required: true },
     posts: { type: [String], ref: "Posts" },
     friends: { type: [Types.ObjectId], ref: "User" },

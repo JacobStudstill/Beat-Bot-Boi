acf1b9758835e6131be980560a511721f27bc0b2
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-10 10:32:51 -0600

    Reply routes

--- server/routes/api/postRoutes.js
+++ server/routes/api/postRoutes.js
@@ -9,6 +9,8 @@ const {
   deleteComment,
 } = require('../../controllers/post-controller.js');
 
+const replyRoutes = require('./replyRoutes');
+
 // /api/posts
 router.route('/').get(getPosts).post(createPost);
 
@@ -20,5 +22,8 @@ router.route('/:postId/comments').post(addComment)
 
 // /api/posts/:postId/comments
 router.route('/:postId/comments/:commentId').delete(deleteComment)
+
+// Import the replies routes
+router.use('/:postId/comments', replyRoutes);
 
 module.exports = router;
---
+++ server/routes/api/replyRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const {
+  addReply,
+  deleteReply,
+  upvoteReply,
+  downvoteReply,
+} = require('../../controllers/replies-controller.js');
+
+// /api/posts/:postId/comments/:commentId/replies
+router.route('/:commentId/replies').post(addReply);
+
+// /api/posts/:postId/comments/:commentId/replies/:replyId
+router.route('/:commentId/replies/:replyId').delete(deleteReply).put(upvoteReply).put(downvoteReply);
+
+module.exports = router;

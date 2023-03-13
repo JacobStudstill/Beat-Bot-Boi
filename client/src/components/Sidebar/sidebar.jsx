b23df8886248b7d19efa06a72b1f71a49ea68d04
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-13 16:32:21 -0600

    Friend/playlist sidebar index

---
+++ client/src/components/Sidebar/sidebar.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import './sidebar.css';
+import profilep from '../../assets/profilep.jpeg'
+import spotify from '../../assets/spotify.jpeg'
+
+
+const SideBar = () => {
+  return (
+    <>
+    <div className="sidebar">
+      <div className="sideContainer">
+        <h4 className="title">User Info</h4>
+        <div className="info">
+          <div className="infoContent">
+            <span className="info1">City:</span>
+            <span className="info2">Melbourne, FL</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="info1">Checkout my playlist!</span>
+            <span className="info2"> <img src={spotify} alt="spotify"></> </span>
+          </div>
+          
+        </div>
+        <h4 className="friends">User friends</h4>
+        <div className="friendList">
+          <div className="friend">
+            <img
+              src={profilep}
+              alt=""
+              className="friendImg"
+            />
+            <span className="friendName">Dom</span>
+          </div>
+          <div className="friend">
+            <img
+              src={profilep}
+              alt=""
+              className="friendImg"
+            />
+            <span className="friendName">Eric</span>
+          </div>
+          <div className="friend">
+            <img
+              src={profilep}
+              alt=""
+              className="friendImg"
+            />
+            <span className="friendName">Jacob</span>
+          </div>
+          <div className="friend">
+            <img
+              src={profilep}
+              alt=""
+              className="friendImg"
+            />
+            <span className="friendName">Alex</span>
+          </div>
+          
+        </div>
+        </div>
+    </div>
+      </>
+    );
+  };
+
+  export default SideBar;

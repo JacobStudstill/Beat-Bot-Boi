fc8da04435773513e1fe2adb7525eac4912e6c6f
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-12 22:07:12 -0600

    message routes

---
+++ server/controllers/message-controller.js
@@ -0,0 +1,16 @@
+const { User, Message } = require("../models");
+const messageController = {
+  // send message
+  async sendMessage (req, res)
+    try {
+
+    } catch {
+
+    }
+
+  // delete a message
+
+  // get all messages between users
+}
+
+module.exports = messageController;

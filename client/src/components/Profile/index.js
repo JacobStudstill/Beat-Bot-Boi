a5aad3c0ece307e45ba8a9a2298f033926940c8e
Alex Nash <Alexr.nash@gmail.com>
2023-03-13 12:02:01 -0400

    set up initial grid

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 const Profile = () => {
   return (
 
-      <div >
-        <h4>Profile Page</h4>
-      </div>
+    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+
+    <Grid xs={6}>
+      <p>1</p>
+    </Grid>
     
+    <Grid xs={6}>
+      <p>2</p>
+    </Grid>
+
+    <Grid xs={6}>
+      <p>3</p>
+    </Grid>
+
+    <Grid xs={6}>
+      <p>4</p>
+    </Grid>
+
+    <Grid xs={6}>
+      <p>4</p>
+    </Grid>
+
+  </Grid>
+
   );
 };
 

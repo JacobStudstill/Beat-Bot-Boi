f7d1fd74f96f688669b6116858150cabbee50a4a
Hezakai <graves.eric@gmail.com>
2023-03-14 11:10:09 -0500

    Upload Functionality implemented

--- server/server.js
+++ server/server.js
@@ -1,27 +1,17 @@
 // Import Express
 const express = require('express');
-// Import DB connection
-const db = require('./config/connection');
-// Import routes
-const routes = require('./routes');
-const uploadRoutes = require('./routes/api/uploadRoutes');
-
-
-
-
-
 // Set up port
 const PORT = process.env.PORT || 3001;
 // Create instance of express app
 const app = express();
+// Import DB connection
+const db = require('./config/connection');
 
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use('/api/upload', uploadRoutes);
-
-// Middleware to parse to json 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Import routes
+const routes = require('./routes');
 app.use(routes);
 
 db.once('open', () => {

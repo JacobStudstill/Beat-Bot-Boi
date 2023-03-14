5bb4bab8307a2090a021bfb39538dfafdeb643b0
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-14 11:08:13 -0600

    added bycrpt to user model and fixed format

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,44 @@
 const { Schema, Types, model } = require('mongoose');
 // const { use } = require('../routes');
 const commentSchema = require('./Comment');
-const replySchema = require('./Reply');
+// const replySchema = require('./Reply');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, validate: {
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+
+    email: { 
+        type: String, 
+        required: true, 
+        validate: {
         validator: function(v) {
           return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
-        },
+            },
         message: props => `${props.value} is not a valid email address!`
-    }, },
-    password: { type: String, required: true },
-    posts: { type: [Types.ObjectId], ref: "Post" },
-    friends: { type: [Types.ObjectId], ref: "User" },
+        }, 
+    },
+
+    password: { 
+        type: String, 
+        required: true, minLength: 8 },
+    
+    posts: { 
+        type: [Types.ObjectId], 
+        ref: "Post" 
+    },
+    
+    friends: { 
+        type: [Types.ObjectId], 
+        ref: "User" 
+    },
     // subs: { type: [Types.ObjectId], ref: "Sub" },
-    comments: { type: [Types.ObjectId], ref: "Comment" }
+    comments: { 
+        type: [Types.ObjectId], 
+        ref: "Comment" }
 },
     {
         toJSON: {
@@ -28,6 +51,21 @@ const userSchema = new Schema({
 userSchema.virtual('userCommentCount').get(function () {
     return this.comments.length
 });
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // compare the incoming password with the hashed password
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
 
 const User = model('User', userSchema)
 module.exports = User;

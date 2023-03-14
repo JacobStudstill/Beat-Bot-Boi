2e352b3f8f1e50f7fc8c3870a396622e4ca9f14e
Alex Nash <Alexr.nash@gmail.com>
2023-03-14 15:22:02 -0400

    started backend for events

---
+++ server/routes/api/eventRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const {
+    getEvents,
+    getEventbyId,
+    updateEvent,
+    deleteEvent,
+    addEvent,
+
+} = require('../../controllers/event-controller');
+
+module.exports = router;

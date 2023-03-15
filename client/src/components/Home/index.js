965cb4363e3ae7c1de3b690fcef209b05257593f
Hezakai <graves.eric@gmail.com>
2023-03-14 20:33:18 -0500

    added live feed to homepage

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,297 +1,92 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Grid from '@mui/material/Grid'; // Grid version 1
-// import Share from '../Share/index'
-// import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Auth from '../../utils/auth';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+const Container = styled('div')({
+  display: 'flex',
+  justifyContent: 'center',
+});
 
-export default function RecipeReviewCard() {
+const StyledCard = styled(Card)({
+  maxWidth: 345,
+  margin: '10px',
+  boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+});
+
+const StyledCardContent = styled(CardContent)({
+  display: 'flex',
+  justifyContent: 'center',
+});
+
+export default function Feed() {
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/posts');
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   const token = Auth.loggedIn() ? Auth.getToken() : null;
   const user = token ? Auth.getProfile().data.username : null;
-  
-  const expanded = React.useState(false);
-// removed setExpanded from const above, was throwing error, may need to replace later. If so, write as const [expanded, setExpanded]
+  const userInit = user ? user[0] : null;
+
+
   return (
-    <>
-    {token && <h1>Welcome {user}!</h1>}
-      {/* <Share /> */}
-      
-      <Grid container spacing={0} columns={12}>
-        <Grid xs={4} display='flex' alignItems='center' direction='column'> 
-        <p>
-        <Button variant="contained">Share</Button>
-
-        </p>
-        </Grid>
-        <Grid xs={4}>
-          <p>
-            <Card sx={{ maxWidth: 750 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image="/static/images/cards/paella.jpg"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                // had to comment out due to errors, may need to figure out later
-                // onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                  aside for 10 minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                  medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                  occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                  large plate and set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                  stirring often until thickened and fragrant, about 10 minutes. Add
-                  saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with artichokes and
-                  peppers, and cook without stirring, until most of the liquid is absorbed,
-                  15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                  mussels, tucking them down into the rice, and cook again without
-                  stirring, until mussels have opened and rice is just tender, 5 to 7
-                  minutes more. (Discard any mussels that don&apos;t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then serve.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
-
-          <Card sx={{ maxWidth: 750 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image="/static/images/cards/paella.jpg"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                // onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                  aside for 10 minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                  medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                  occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                  large plate and set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                  stirring often until thickened and fragrant, about 10 minutes. Add
-                  saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with artichokes and
-                  peppers, and cook without stirring, until most of the liquid is absorbed,
-                  15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                  mussels, tucking them down into the rice, and cook again without
-                  stirring, until mussels have opened and rice is just tender, 5 to 7
-                  minutes more. (Discard any mussels that don&apos;t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then serve.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
-
-          <Card sx={{ maxWidth: 750 }}>
-            <CardHeader
-              avatar={
-                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                  R
-                </Avatar>
-              }
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title="Shrimp and Chorizo Paella"
-              subheader="September 14, 2016"
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image="/static/images/cards/paella.jpg"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                // onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                  aside for 10 minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                  medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                  occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                  large plate and set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                  stirring often until thickened and fragrant, about 10 minutes. Add
-                  saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with artichokes and
-                  peppers, and cook without stirring, until most of the liquid is absorbed,
-                  15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                  mussels, tucking them down into the rice, and cook again without
-                  stirring, until mussels have opened and rice is just tender, 5 to 7
-                  minutes more. (Discard any mussels that don&apos;t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then serve.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
-          </p>
-        </Grid>
-        <Grid xs={4} display='flex' alignItems='center' direction='column' >
-
-          <p>
-          <Button variant="contained">Communities</Button>
-          </p>
-
-          <p>
-          <Button variant="contained">Upcoming Shows</Button>
-          </p>
-
-        </Grid>
-      </Grid>
-    </>
-  )
-};
-
-
+    <Container>
+      <div>
+        {token && <h1>Welcome {user}!</h1>}
+        <h1>Feed</h1>
+        {posts.length > 0 ? (
+          posts.map((post) => {
+            let videoUrl = '';
+            if (post.postLink && post.postLink.includes("youtube.com") && post.postLink.includes("watch")) {
+              videoUrl = post.postLink.replace("watch?v=", "embed/");
+            } else if (post.postLink && post.postLink.includes("youtube.com") && post.postLink.includes("embed/")) {
+              videoUrl = post.postLink;
+            }
+            return (
+              <div key={post._id}>
+                <Link to={`/posts/${post._id}`}>
+                  <StyledCard>
+                    <CardHeader
+                      avatar={
+                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                          {post.username[0]}
+                        </Avatar>
+                      }
+                      title={post.postTitle}
+                      subheader={new Date(post.createdAt).toLocaleDateString()}
+                    />
+                    <StyledCardContent>
+                      {videoUrl && <iframe src={videoUrl}></iframe>}
+                      <Typography variant="body2" color="text.secondary">
+                        {post.postText}
+                      </Typography>
+                    </StyledCardContent>
+                  </StyledCard>
+                </Link>
+              </div>
+            );
+          })
+        ) : (
+          <p>Loading posts...</p>
+        )}
+      </div>
+    </Container>
+  );
+}

44b89c9d75cb39cf0b71f8e49714180a8e38548f
Hezakai <graves.eric@gmail.com>
2023-03-15 15:05:00 -0500

    fixed post and comment rendering

--- client/src/components/Posts/index.js
+++ client/src/components/Posts/index.js
@@ -1,94 +1,102 @@
 import * as React from 'react';
+import { useParams } from 'react-router-dom';
 import Auth from '../../utils/auth';
 
-export default function PostDetail() {
-    const [post, setPost] = React.useState(null);
-    const [loading, setLoading] = React.useState(true);
+function Comment({ comment }) {
+  const [nestedComments, setNestedComments] = React.useState([]);
 
-    React.useEffect(() => {
-        const fetchPost = async () => {
-            try {
-                let postId = '640bb7b478b224e68117d6a6';
-                const response = await fetch('/api/posts/' + postId);
-                const data = await response.json();
-                console.log(data);
-                setPost(data);
-                setLoading(false);
-            } catch (error) {
-                console.error(error);
-                setLoading(false);
-            }
-        };
-        fetchPost();
-    }, []);
+  React.useEffect(() => {
+    const fetchNestedComments = async () => {
+      try {
+        const nestedCommentIds = comment?.comments || [];
+        const nestedCommentPromises = nestedCommentIds.map(async (commentId) => {
+          const response = await fetch('/api/comments/' + commentId);
+          const data = await response.json();
+          return data;
+        });
+        const nestedComments = await Promise.all(nestedCommentPromises);
+        setNestedComments(nestedComments);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchNestedComments();
+  }, [comment]);
 
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-    const user = token ? Auth.getProfile().data.username : null;
-
-    if (loading) {
-        return <h2>Loading...</h2>;
-    }
-
-    if (!post) {
-        return <h2>No post found</h2>;
-    }
-
-    return (
-        <div>
-          <h1>{post.postTitle}</h1>
-          <p>Username: {post.username}</p>
-          <p>Created At: {post.createdAt}</p>
-          <p>Post Link: <a href={post.postLink}>{post.postLink}</a></p>
-          <p>Tags: {post.tags.join(', ')}</p>
-          <p>Post Upvotes: {post.postUpvotes}</p>
-          <p>Post Downvotes: {post.postDownvotes}</p>
-          <h2>Comments ({post.comments.length})</h2>
-          {post.comments.map(commentId => (
-            <div key={commentId}>
-              <Comment commentId={commentId} />
-            </div>
-          ))}
-        </div>
-    );
+  return (
+    <div style={{ marginLeft: 20 }}>
+      <p>Username: {comment.username}</p>
+      <p>Comment Body: {comment.commentBody}</p>
+      {nestedComments.map((nestedComment) => (
+        <Comment key={nestedComment._id} comment={nestedComment} />
+      ))}
+    </div>
+  );
 }
 
-function Comment({ commentId }) {
-    const [comment, setComment] = React.useState(null);
-    const [loading, setLoading] = React.useState(true);
+export default function PostDetail() {
+  const [post, setPost] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [comments, setComments] = React.useState([]);
+  const { postId } = useParams();
 
-    React.useEffect(() => {
-        const fetchComment = async () => {
-            try {
-                const response = await fetch('/api/comments/' + commentId);
-                const data = await response.json();
-                console.log(data);
-                setComment(data);
-                setLoading(false);
-            } catch (error) {
-                console.error(error);
-                setLoading(false);
-            }
-        };
-        fetchComment();
-    }, [commentId]);
+  React.useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch('/api/posts/' + postId);
+        const data = await response.json();
+        setPost(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    fetchPost();
+  }, [postId]);
 
-    if (loading) {
-        return <h3>Loading comment...</h3>;
-    }
+  React.useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const commentIds = post?.comments || [];
+        const commentPromises = commentIds.map(async (commentId) => {
+          const response = await fetch('/api/comments/' + commentId);
+          const data = await response.json();
+          return data;
+        });
+        const comments = await Promise.all(commentPromises);
+        setComments(comments);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchComments();
+  }, [post]);
 
-    if (!comment) {
-        return <h3>No comment found with ID {commentId}</h3>;
-    }
+  const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const user = token ? Auth.getProfile().data.username : null;
 
-    return (
-        <div style={{ marginLeft: '20px' }}>
-          <h4> {comment.commentBody}</h4>
-          <h5>{comment.username}</h5>
-          {comment.comments.map(nestedCommentId => (
-            <div key={nestedCommentId}>
-              <Comment commentId={nestedCommentId} />
-            </div>
-          ))}
-        </div>
-    );
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (!post) {
+    return <h2>No post found</h2>;
+  }
+
+  return (
+    <div>
+      <h1>{post.postTitle}</h1>
+      <p>Username: {post.username}</p>
+      <p>Created At: {post.createdAt}</p>
+      <p>Post Link: <a href={post.postLink}>{post.postLink}</a></p>
+      <p>Tags: {post.tags.join(', ')}</p>
+      <p>Post Upvotes: {post.postUpvotes}</p>
+      <p>Post Downvotes: {post.postDownvotes}</p>
+      <h2>Comments ({comments.length})</h2>
+      {comments.map((comment) => (
+        <Comment key={comment._id} comment={comment} />
+      ))}
+    </div>
+  );
 }

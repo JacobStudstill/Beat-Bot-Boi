6dec430abf2912085c7432bbdbcd0453050ad7e9
Alex Nash <Alexr.nash@gmail.com>
2023-03-15 17:28:51 -0400

    removed setting dropdown

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -23,29 +23,21 @@ import { Link } from 'react-router-dom'
 import Auth from '../../utils/auth'
 import logo from '../../assets/BeatBotzLogo.PNG'
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout', 'Message'];
 
 function Header() {
 
   const token = Auth.loggedIn() ? Auth.getToken() : null
   const user = token ? Auth.getProfile().data.username : null
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   const loggedIn = ['Home', 'Profile', 'Logout']
   const loggedOut = ['Login']
@@ -121,6 +113,7 @@ function Header() {
                 ))};
             </Menu>
           </Box>
+          
           <Typography
             variant="h5"
             noWrap
@@ -160,37 +153,7 @@ function Header() {
                 </Button>
               ))}
           </Box>
-          
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          
+
           {/* Search Bar */}
           <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
             <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />

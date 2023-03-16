1f0b2201c79880b291b3cb032b4d050ef66f779c
Hezakai <graves.eric@gmail.com>
2023-03-15 20:43:59 -0500

    added karma and replies to comments

--- client/src/components/Posts/view.js
+++ client/src/components/Posts/view.js
@@ -1,6 +1,43 @@
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+
+const Container = styled('div')({
+  display: 'flex',
+  justifyContent: 'center',
+});
+
+const StyledCard = styled(Card)({
+  maxWidth: '800px',
+  minHeight: '300px',
+  margin: '10px',
+  boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+});
+
+const StyledCardContent = styled(CardContent)({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+});
+
+const CardText = styled(Typography)({
+  textDecoration: 'none',
+});
+
+// Define a new styled component for the iframe
+const StyledIframe = styled("iframe")({
+  width: "100%",
+  height: "500px", // Update the height to your desired value
+});
 
 function Comment({ comment }) {
   const [nestedComments, setNestedComments] = React.useState([]);
@@ -25,8 +62,9 @@ function Comment({ comment }) {
 
   return (
     <div style={{ marginLeft: 20 }}>
-      <p>Username: {comment.username}</p>
-      <p>Comment Body: {comment.commentBody}</p>
+      <p>{comment.username}</p>
+      <p>"{comment.commentBody}"</p>
+      <p>👍: {comment.commentUpvotes} 👎: {comment.commentDownvotes} Replies ({comment.comments.length})</p>
       {nestedComments.map((nestedComment) => (
         <Comment key={nestedComment._id} comment={nestedComment} />
       ))}
@@ -87,12 +125,13 @@ export default function PostDetail() {
   return (
     <div>
       <h1>{post.postTitle}</h1>
-      <p>Username: {post.username}</p>
-      <p>Created At: {post.createdAt}</p>
-      <p>Post Link: <a href={post.postLink}>{post.postLink}</a></p>
+      <p>Posted By: {post.username}</p>
+      <p>Created At: {new Date(post.createdAt).toLocaleString([], { hour: 'numeric', minute: 'numeric', hour12: true })
+      }</p>
       <p>Tags: {post.tags.join(', ')}</p>
-      <p>Post Upvotes: {post.postUpvotes}</p>
-      <p>Post Downvotes: {post.postDownvotes}</p>
+      <p>Post Link: <a href={post.postLink}>{post.postLink}</a></p>
+      <p>Post Text: {post.postText}</p>
+      <p>👍: {post.postUpvotes} 👎: {post.postDownvotes}</p>
       <h2>Comments ({comments.length})</h2>
       {comments.map((comment) => (
         <Comment key={comment._id} comment={comment} />

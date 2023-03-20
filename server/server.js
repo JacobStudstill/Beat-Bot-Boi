481878835778750b03c45645166aa87972c3b451
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-20 12:09:55 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
